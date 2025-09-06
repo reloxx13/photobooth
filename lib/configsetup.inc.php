323e10a5e88995faaddf060860d6ef772c8e41f8
Andreas Skomski <andi@unlegacy-android.org>
2025-09-06 21:32:22 +0200

    api(print): add endpoint to increase limit
    - added new API endpoint to increase the print limit and unlock
    - added hidden config to store Photobooth URL
    
    Change-Id: I9cdda9a1aa1f7bb55b21d68ca82cce235be17b3a

--- lib/configsetup.inc.php
+++ lib/configsetup.inc.php
@@ -219,6 +219,11 @@ return [
             'name' => 'dev[reload_on_error]',
             'value' => $config['dev']['reload_on_error'],
         ],
+        'webserver_url' => [
+            'type' => 'hidden',
+            'name' => 'webserver[url]',
+            'value' => $config['webserver']['url'],
+        ],
         'webserver_ssid' => [
             'view' => 'expert',
             'type' => 'input',
