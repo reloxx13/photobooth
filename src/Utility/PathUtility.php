309f4d205cf0632d243c01f0a957cbb10f17098e
reloxx13 <reloxx@interia.pl>
2025-11-29 16:33:46 +0100

    fix tests

--- src/Utility/PathUtility.php
+++ src/Utility/PathUtility.php
@@ -149,7 +149,7 @@ class PathUtility
      */
     public static function getBaseUrl(): string
     {
-        $documentRoot = realpath($_SERVER['DOCUMENT_ROOT']);
+        $documentRoot = (string)realpath($_SERVER['DOCUMENT_ROOT']);
         $rootPath = self::getRootPath();
 
         return self::fixFilePath(str_replace($documentRoot, '', $rootPath) . '/');
